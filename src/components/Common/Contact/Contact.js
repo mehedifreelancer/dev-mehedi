6568e3f8201a555cf12a9579d658e636f38b424e
mehed-hasan <mehedi.hasan@intelli.global>
2022-10-15 13:55:58 +0600

    --

--- src/components/Common/Contact/Contact.js
+++ src/components/Common/Contact/Contact.js
@@ -11,18 +11,18 @@ function Contact() {
                     <div className="contact_info">
                         <div className="info_item">
                             <i className="lnr lnr-home"></i>
-                            <h6>California, United States</h6>
-                            <p>Santa monica bullevard</p>
+                            <h6>Dhaka,Bangladesh</h6>
+                            <p>Nadda, Baridhara</p>
                         </div>
                         <div className="info_item">
                             <i className="lnr lnr-phone-handset"></i>
-                            <h6><a href="#">00 (440) 9865 562</a></h6>
-                            <p>Mon to Fri 9am to 6 pm</p>
+                            <h6><a href="#">01781873064</a></h6>
+                            <p>Don't forget to knock through mobile</p>
                         </div>
                         <div className="info_item">
                             <i className="lnr lnr-envelope"></i>
-                            <h6><a href="#">support@colorlib.com</a></h6>
-                            <p>Send us your query anytime!</p>
+                            <h6><a href="#">mehediaiyub451@gmail.com</a></h6>
+                            <p>Send me your mail anytime!</p>
                         </div>
                     </div>
                 </div>
