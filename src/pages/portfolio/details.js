b26352adf4373fee3b3046be36971587e3b26db1
Mehedi Hasan <39464059+mehedifreelancer@users.noreply.github.com>
2023-01-06 22:16:30 +0600

    Update details.js

--- src/pages/portfolio/details.js
+++ src/pages/portfolio/details.js
@@ -41,7 +41,6 @@ function PortfolioDetails() {
                     <div className="offset-lg-1 col-lg-5">
                         <div className="portfolio_right_text mt-30">
                             <h4 className="text-uppercase">{title}</h4>
-			    <div  dangerouslySetInnerHTML={{__html: data.desc}}> </div>
                             <ul className="list">
                                 <li><span>Stacks</span>:
                                 {
